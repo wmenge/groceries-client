0954a299fb2e5434ef855b8b274a7e64c4c0ede2
Wilco Menge <wilcomenge@gmail.com>
2025-01-28 15:30:35 +0100

    cleanup

---
+++ src/components/GroceryList.vue
@@ -0,0 +1,33 @@
+<script setup lang="js">
+import { ref, onMounted } from 'vue'
+import { groceryResource } from '../util/entityResource.js';
+
+const groceriesList = ref(null)
+
+onMounted(() => {
+  fetchData();
+})
+
+async function fetchData() {
+    try {
+    groceriesList.value = await groceryResource.getAll();
+  } catch (err) {
+    //error.value = err.toString()
+  } finally {
+    //loading.value = false
+  }
+}
+
+</script>
+
+<template>
+  <ul>
+      <li v-for="grocery in groceriesList" :key="grocery.id">
+        {{ grocery.name }}
+      </li>
+    </ul>
+</template>
+
+<style scoped>
+
+</style>
