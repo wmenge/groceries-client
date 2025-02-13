117716569e18a4956c7e63e6933fd8e29a7dc467
Wilco Menge <wilcomenge@gmail.com>
2025-02-13 11:12:38 +0100

    Add support for Google authentication

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,11 +5,26 @@
 
 <template>
 
-  Login with:
-  <a :href="`${ apiUrl }/auth/github/redirect`">Github</a>
+<div class="jumbotron">
+  <h1 class="display-6">Please log in...</h1>
+  <hr class="my-4">
+  <p class="lead">
+    
+    <a class="btn btn-primary btn-lg" :href="`${ apiUrl }/auth/google/redirect`">Google</a> &nbsp;
+    <a class="btn btn-secondary btn-lg" :href="`${ apiUrl }/auth/github/redirect`">Github</a>
+    
+  </p>
+</div>
 
 </template>
 
 <style scoped>
+
+.jumbotron {
+  padding: 4rem 2rem;
+  background-color: #eeeeee;
+  border-radius: .5rem;
+}
+
 
 </style>
