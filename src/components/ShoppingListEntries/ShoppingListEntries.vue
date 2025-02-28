04483de9ef32dd77bb6d2699587da7dac800706d
Wilco Menge <wilcomenge@gmail.com>
2025-02-28 16:46:55 +0100

    first css animation

--- src/components/ShoppingListEntries/ShoppingListEntries.vue
+++ src/components/ShoppingListEntries/ShoppingListEntries.vue
@@ -11,9 +11,11 @@ function handleEntryModified(entry) {
 </script>
 
 <template>
-  <ul class="list-group">
-    <ShoppingListEntriesListItem v-for="shoppingListEntry in props.shoppingListEntries" :key="shoppingListEntry.id" @entryModified="handleEntryModified" :shoppingListEntry="shoppingListEntry"></ShoppingListEntriesListItem>
-  </ul>
+  <TransitionGroup class="list-group" name="list" tag="ul">
+    <!-- <ul> -->
+      <ShoppingListEntriesListItem v-for="shoppingListEntry in props.shoppingListEntries" :key="shoppingListEntry.id" @entryModified="handleEntryModified" :shoppingListEntry="shoppingListEntry"></ShoppingListEntriesListItem>
+    <!-- </ul> -->
+  </TransitionGroup>
 </template>
 
 <style scoped>
@@ -25,5 +27,25 @@ a {
 
 input#quantity {
   flex: 0 0 3rem;
+}
+
+/* animation */
+
+.list-move, /* apply transition to moving elements */
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+  position: absolute;
 }
 </style>
